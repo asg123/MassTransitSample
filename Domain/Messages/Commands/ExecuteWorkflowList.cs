33f96d2238f069391e0e6a56407f0be86635b651
Braulio Megias <braulio.megias@gmail.com>
2013-04-25 16:11:14 +0200

    added ExecuteWorkflowList class example

---
+++ Domain/Messages/Commands/ExecuteWorkflowList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.Messages.Commands
+{
+
+    public class ExecuteWorkflowList
+    {
+        public Guid BatchId { get; set; }
+        public IEnumerable<WorkflowExecution> WorkflowList;
+    }
+
+    public class WorkflowExecution
+    {
+        public string WorkflowId { get; set; }
+        public IEnumerable<WorkflowParameter> Parameters { get; set; }
+    }
+
+    public class WorkflowParameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+}
